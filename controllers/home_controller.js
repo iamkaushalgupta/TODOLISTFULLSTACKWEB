61c02b2f16a66724ad4cb382f27cf4ef091cf79a
Kaushal Gupta <kaushalgupta@kaushalss102.local>
2023-01-07 16:50:50 +0530

    Frontend is completed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,14 @@
 module.exports.home = function(req,res){
     return res.render('home',{
-        title:'Home'
+        title:'Home',
+        descriptionText:'Description',
+        descriptionPlaceholder:'What you need to do?',
+        categoryText: 'Category',
+        choseCategoryText: 'Choose Category Text',
+        categoryList:['Personal', 'Work', 'School', 'Cleaning', 'Others' ],
+        dueDateText: 'Due Date',
+        button1:'Add Tasks',
+        button2:'Delete Tasks'
+        
     })
 }
