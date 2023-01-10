ed828b6a0d0956b896914b0bc2272d7d2527ecf5
Kaushal Gupta <kaushalgupta@kaushalss102.local>
2023-01-10 14:40:05 +0530

    Add Todo in the database

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,23 @@
+
+
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title:'Home',
-        descriptionText:'Description',
-        descriptionPlaceholder:'What you need to do?',
-        categoryText: 'Category',
-        choseCategoryText: 'Choose Category Text',
-        categoryList:['Personal', 'Work', 'School', 'Cleaning', 'Others' ],
-        dueDateText: 'Due Date',
-        button1:'Add Tasks',
-        button2:'Delete Tasks'
-        
+    
+    const TODO = require('../models/models');
+    TODO.find({},function(err,TODOs){
+  
+        return res.render('home',{
+            title:'Home',
+            descriptionText:'Description',
+            descriptionPlaceholder:'What you need to do?',
+            categoryText: 'Category',
+            choseCategoryText: 'Choose Category Text',
+            categoryList:['Personal', 'Work', 'School', 'Cleaning', 'Others' ],
+            dueDateText: 'Due Date',
+            button1:'Add Tasks',
+            button2:'Delete Tasks',
+            TODOs:TODOs
+        })
     })
+
+    
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
+const { application } = require('express');
 const express= require('express');
-
+const TODO = require('../models/models');
 const routes = express.Router();
 const homeController = require('../controllers/home_controller');
-
+routes.use(express.urlencoded())
 
 routes.get('/',homeController.home);
 
+routes.post('/add-task',function(req,res){
 
+    TODO.create({
+        description:req.body.description,
+        category:req.body.category,
+        date:req.body.date,
+
+    },function(err, newTODO){
+        if(err){
+            console.log(err);
+            return;
+        }
+        res.redirect('back');
+    })
+
+})
 
 module.exports=routes;
